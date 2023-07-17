b3f610e79fe3a6bc614c0676c21b063501a9fda0
Rocio Sanabria <rogsanabria@gmail.com>
2023-07-17 02:54:05 -0300

    PrimeraEntrega

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting={'Bienvenidos a Kü'} />
+
+    </div>
+    
+  );
+}
+
+export default App;
