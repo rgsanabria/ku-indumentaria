793b0376fb061c02c63f1363035c1fad1d0251ba
Rocio Sanabria <rogsanabria@gmail.com>
2023-07-26 23:36:39 -0300

    updateNavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,21 @@
+import './NavBar.css'
 import CartWidget from "../CartWidget/CartWidget";
+import { NavLink, Link } from 'react-router-dom'
 
 
 const NavBar = () => {
     return (
-        <nav>
-            <h3>Ecommerce</h3>
-            <div>
+        <nav className="NavBar">
+            <Link to='/'>
+            <h3>Ku</h3>
+            </Link>
+            
+            <div className='Categories'>
                 <button>Inicio</button>
                 <button>Productos</button>
-                <button>Envios</button>
-                <button>Contactos</button>
+                <NavLink to={`/category/buzo`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></NavLink>
+                <NavLink to={`/category/buzoOver`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></NavLink>
+                
         
             </div>
             <CartWidget />
